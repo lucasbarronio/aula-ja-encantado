790693174c8e20c5c065542a6696f3efabc5d6da
lucasbarronio <lucas.barronio@universo.univates.br>
2024-06-14 10:57:22 -0300

    Alteração de sintaxe

--- aula-07jun/script.js
+++ aula-07jun/script.js
@@ -5,11 +5,9 @@ const lst = document.getElementById("lista");
 btn.addEventListener('click', function () {
   let item = document.createElement('LI');
   let texto = document.createTextNode(ipt.value);
-  item.appendChild(texto);
-
-  lst.appendChild(item);
+  item.append(texto);
+  lst.append(item);
   ipt.value = "";
-
 })
 
 
