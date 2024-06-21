cbca6d605b294a8c3a66c30704240279a886183d
lucasbarronio <lucas.barronio@universo.univates.br>
2024-06-21 16:46:21 -0300

    Versionamento aula 21-06

---
+++ aula-21jun/script.js
@@ -0,0 +1,48 @@
+// const dados = [
+//     {
+//         "userId": 1,
+//         "id": 1,
+//         "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+//         "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+//     },
+//     {
+//         "userId": 1,
+//         "id": 2,
+//         "title": "qui est esse",
+//         "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+//     },
+//     {
+//         "userId": 1,
+//         "id": 3,
+//         "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+//         "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+//     }]
+
+
+async function main() {
+    const response = await fetch('https://brasilapi.com.br/api/ibge/uf/v1');
+    const dados = await response.json();
+
+    const tabela = document.getElementById('tabela');
+    const corpo_tabela = document.getElementById('corpo_tabela');
+
+    dados.forEach(element => {
+        const trow = document.createElement('tr');
+        corpo_tabela.appendChild(trow);
+        const td = document.createElement('td');
+        const td1 = document.createElement('td');
+        const td2 = document.createElement('td');
+        td.textContent = element.id;
+        td1.textContent = element.nome;
+        td2.textContent = element.sigla;
+        trow.appendChild(td);
+        trow.appendChild(td1);
+        trow.appendChild(td2);
+        const td3 = document.createElement('td');
+        td3.textContent = element.regiao.nome;
+        trow.appendChild(td3);
+    });
+}
+
+main();
+
