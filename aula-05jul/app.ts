94c31ab955612b5225c3b3bd98f1f5ec34fd8f59
lucasbarronio <lucas.barronio@universo.univates.br>
2024-07-05 16:25:12 -0300

    API - Put

--- aula-05jul/app.ts
+++ aula-05jul/app.ts
@@ -25,28 +25,47 @@ app.get('/bebidas/:id', (req: Request, res: Response): Response => {
 
 app.delete('/bebidas/:id', (req: Request, res: Response): Response => {
   const id = Number(req.params.id);
-  const bebida = copa.recuperarUm(id);
+  const bebida: Bebida | undefined = copa.recuperarUm(id);
   if (bebida) {
     const excluiu = copa.excluir(bebida);
     if (excluiu) {
-      return res.status(200).json({message: 'Bebida excluída com sucesso.'});
+      return res.status(200).json({ message: 'Bebida excluída com sucesso.' });
     } else {
-      return res.status(500).json({message: 'Erro ao excluir bebida.'});
+      return res.status(500).json({ message: 'Erro ao excluir bebida.' });
     }
   } else {
-    return res.status(404).json({message: 'Bebida não encontrada.'});
+    return res.status(404).json({ message: 'Bebida não encontrada.' });
   }
 })
 
+app.put('/bebidas/:id', (req: Request, res: Response): Response => {
+  const id = Number(req.params.id);
+  const bebida: Bebida | undefined = copa.recuperarUm(id);
+  if (bebida) {
+    let novaB: Bebida = new Bebida;
+    novaB.cor = req.body.cor;
+    novaB.nome = req.body.nome;
+    novaB.temperatura = req.body.temperatura;
+    novaB.teorAlcool = req.body.teorAlcool;
+    novaB.quantidade = req.body.quantidade;
+    copa.editar(bebida, novaB);
+    return res.status(200).json(novaB);
+  } else {
+    return res.status(404).json({ message: 'Bebida nao encontrada.' });
+  }
+
+
+})
+
 app.post('/bebidas', (req: Request, res: Response): Response => {
-const bebida: Bebida = new Bebida();
-bebida.cor = req.body.cor;
-bebida.nome = req.body.nome;
-bebida.quantidade = req.body.quantidade;
-bebida.teorAlcool = req.body.teorAlcool;
-bebida.temperatura = req.body.temperatura;
-copa.salvar(bebida);
-return res.status(200).json(bebida);
+  const bebida: Bebida = new Bebida();
+  bebida.cor = req.body.cor;
+  bebida.nome = req.body.nome;
+  bebida.quantidade = req.body.quantidade;
+  bebida.teorAlcool = req.body.teorAlcool;
+  bebida.temperatura = req.body.temperatura;
+  copa.salvar(bebida);
+  return res.status(200).json(bebida);
 })
 
 app.listen(port, () => {
