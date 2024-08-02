12cbecbfd5da21a6b728e1ab23bc27a9e99728ed
lucasbarronio <lucas.barronio@universo.univates.br>
2024-08-02 16:34:38 -0300

    Versiona backend de mecanica

---
+++ aula-26jul-mecanica/backend/src/app.ts
@@ -0,0 +1,22 @@
+import express, { Express, NextFunction, Request, Response } from 'express';
+import cors from 'cors';
+import pessoasRouter from './Routes/pessoa';
+import carrosRouter from './Routes/carro';
+
+const app: Express = express();
+const port: number = 3000
+
+app.use(cors());
+app.use(express.json());
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(`[${new Date()}] ${req.ip} ${req.method} ${req.url}`);
+  next();
+});
+
+app.use(carrosRouter);
+app.use(pessoasRouter);
+
+
+app.listen(port, () => {
+  console.log(`Servidor iniciado em http://localhost:${port}`);
+});
