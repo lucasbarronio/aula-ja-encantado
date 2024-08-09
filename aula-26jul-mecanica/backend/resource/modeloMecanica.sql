1937e504406793b88edebc3ee9cfdf760f595d0a
lucasbarronio <lucas.barronio@universo.univates.br>
2024-08-09 10:26:24 -0300

    Atualização de recursos do banco de dados

--- aula-26jul-mecanica/backend/resource/modeloMecanica.sql
+++ aula-26jul-mecanica/backend/resource/modeloMecanica.sql
@@ -1,25 +1,18 @@
-CREATE TABLE carros (
-  id serial PRIMARY KEY,
-  marca varchar(255) NOT NULL,
-  modelo varchar(255) NOT NULL,
-  cor varchar(55) NOT NULL,
-  categoria varchar(255) NOT NULL,
-  dataFabricacao char(4) NOT NULL,
-  problema varchar(255) NOT NULL
-)
+CREATE TABLE "carros" (
+  "id" serial PRIMARY KEY,
+  "marca" varchar(255) NOT NULL,
+  "modelo" varchar(255) NOT NULL,
+  "cor" varchar(55) NOT NULL,
+  "categoria" varchar(255) NOT NULL,
+  "dataFabricacao" char(4) NOT NULL,
+  "problema" varchar(255) NOT NULL
+);
 
-CREATE TABLE pessoas (
-  id serial PRIMARY KEY,
-  nome varchar(255) NOT NULL,
-  endereco varchar(255) NOT NULL,
-  cpf varchar(11) NOT NULL,
-  email varchar(55) NOT NULL,
-  genero varchar(55) NOT NULL
-)
-
--- SELECT * FROM pessoas
--- SELECT * FROM carros
-
--- ALTER TABLE carros
--- RENAME COLUMN dataFabricacao to "dataFabricacao"; 
-
+CREATE TABLE "pessoas" (
+  "id" serial PRIMARY KEY,
+  "nome" varchar(255) NOT NULL,
+  "endereco" varchar(255) NOT NULL,
+  "cpf" varchar(11) NOT NULL,
+  "email" varchar(55) NOT NULL,
+  "genero" varchar(55) NOT NULL
+);
