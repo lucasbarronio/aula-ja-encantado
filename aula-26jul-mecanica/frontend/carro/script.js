8e28cecd79ae1f41693b25366dffa3fffb3124f4
lucasbarronio <lucas.barronio@universo.univates.br>
2024-08-15 15:26:05 -0300

    Tabela flex e estilização de botões

--- aula-26jul-mecanica/frontend/carro/script.js
+++ aula-26jul-mecanica/frontend/carro/script.js
@@ -1,6 +1,6 @@
 async function excluir(id) {
-    await fetch(`http://localhost:3000/carros/${id}`, { 
-        method: 'DELETE' 
+    await fetch(`http://localhost:3000/carros/${id}`, {
+        method: 'DELETE'
     });
     window.location.reload();
 }
@@ -9,10 +9,10 @@ async function listarCarros() {
     const response = await fetch(`http://localhost:3000/carros`);
     const dados = await response.json();
     const corpoTabela = document.getElementById('corpo-tabela');
-    
+
     dados.forEach(item => {
         const linha = document.createElement('tr');
-        
+
         const tdId = document.createElement('td');
         tdId.textContent = item.id;
         linha.appendChild(tdId);
@@ -20,19 +20,19 @@ async function listarCarros() {
         const tdMarca = document.createElement('td');
         tdMarca.textContent = item.marca;
         linha.appendChild(tdMarca);
-        
+
         const tdModelo = document.createElement('td');
         tdModelo.textContent = item.modelo;
         linha.appendChild(tdModelo);
-        
+
         const tdCor = document.createElement('td');
         tdCor.textContent = item.cor;
         linha.appendChild(tdCor);
-        
+
         const tdCategoria = document.createElement('td');
         tdCategoria.textContent = item.categoria;
         linha.appendChild(tdCategoria);
-        
+
         const tdDataFabricacao = document.createElement('td');
         tdDataFabricacao.textContent = item.data_fabricacao;
         linha.appendChild(tdDataFabricacao);
@@ -42,9 +42,11 @@ async function listarCarros() {
         linha.appendChild(tdProblema);
 
         const tdAcoes = document.createElement('td');
-        tdAcoes.innerHTML = `<button><a href="cadastro.html?id=${item.id}">Editar</a></button>
+        tdAcoes.classList.add("acoes");
+        tdAcoes.innerHTML = `<button  class="botaoeditar"><a href="cadastro.html?id=${item.id}">Editar</a></button>
       <button onclick="excluir(${item.id})">Excluir</button>`;
         linha.appendChild(tdAcoes);
+
 
         corpoTabela.appendChild(linha);
 
